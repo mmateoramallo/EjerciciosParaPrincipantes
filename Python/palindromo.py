2856b382d97725019e42e4c7b78e78c26a78e95e
Mateo <mateo.j.ramallo@gmail.com>
2023-12-30 22:15:06 -0300

    Modificacion

--- Python/palindromo.py
+++ Python/palindromo.py
@@ -1,4 +1,4 @@
-
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida. | Y muestra si es palindromo
 def encontrarPalindromo(cadena):
     n = len(cadena)
     cadenaDos = ""
@@ -10,16 +10,25 @@ def encontrarPalindromo(cadena):
         palindromo = True
     
     
-    return palindromo
+    return palindromo, cadenaDos
 
-
-
+#Escribir un programa que pregunte el correo electrónico del usuario en la consola y muestre por pantalla otro correo electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio ceu.es.
+def correoElectronico(email):
+    return(email[:email.find('@')] + '@ceu.es')
 
 def main():
     print('-'*21, 'Palindromo', '-'*21)
     cadena = input('Ingrese la cadena a buscar el palindromo:')
-    esPalidromo  = encontrarPalindromo(cadena)
-    print(f"La palabra ingresada: {cadena}. Es palidromo?: {esPalidromo}")
+    esPalidromo,cadenaInvertida  = encontrarPalindromo(cadena)
+
+    print(f"La palabra ingresada: {cadena}, tiene una cadena invertida: {cadenaInvertida} . Es palidromo?: {esPalidromo}")
+
+    print('-'*21, 'Correo Modificado', '-'*21)
+
+    correo = input('Ingrese su correo Electronico: ')
+
+    correoModificado = correoElectronico(correo)
+    print(f"El correo electronico original es: {correo} | Y el institucional: {correoModificado}")
     
 
 
